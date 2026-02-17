02e1eb222b2bcb77e26b150cf1009982007d3247
“JingleH” <jingleh321@gmail.com>
2026-02-17 16:58:25 -0600

    update contentRoot

--- education/scripts/scripts.js
+++ education/scripts/scripts.js
@@ -21,7 +21,7 @@ const LIBS = '/libs';
 
 const CONFIG = {
   codeRoot: '/education',
-  // contentRoot: '',
+  contentRoot: '/education',
   imsClientId: 'acom-education',
   imsScope: 'AdobeID,openid,gnav,pps.read,firefly_api,additional_info.roles,read_organizations,account_cluster.read',
   // geoRouting: 'off',
